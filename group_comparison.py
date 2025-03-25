e76aaf7ac8b3393e07b6a9ff4f38600876645950
packmad <simone.aonzo@gmail.com>
2025-03-25 19:54:29 +0100

    Mann-Whitney

--- group_comparison.py
+++ group_comparison.py
@@ -2,8 +2,9 @@
 
 import statistics
 import numpy as np
-from scipy.stats import shapiro, ttest_ind, pearsonr, levene
+
 from typing import Optional
+from scipy.stats import shapiro, ttest_ind, pearsonr, levene, mannwhitneyu
 
 
 def calculate_statistics(data, dec_num: int = 2):
@@ -16,13 +17,31 @@ def calculate_statistics(data, dec_num: int = 2):
     print(f"Avg: {avg}, StDev: {stddev}, Med: {median}, Max: {max_value}, Min: {min_value}")
 
 
+def pearson_correlation(group1, group2, alpha: float = 0.05) -> Optional[bool]:
+    assert len(group1) == len(group2)
+    if is_data_normally_distributed(group1) and is_data_normally_distributed(group2):
+        r, p_value = pearsonr(group1, group2)
+        print(f"Pearson Correlation = {r}, p-value = {p_value}")
+        if p_value >= alpha:
+            print("NO significant correlation between variables.")
+            return False
+        else:
+            print("Significant correlation between variables.")
+            print(f"r{effect_size_interpretation_funder_and_ozer(r)}")
+            return True
+    print('Data is not normally distributed')
+    return None
+
+
 def is_data_normally_distributed(data, alpha: float = 0.05) -> bool:
+    assert 6 < len(data) < 50  # Not recommended otherwise
     s, p = shapiro(data)  # Null hypothesis: data was drawn from a normal distribution
     if p > alpha:
-        print("Fail to reject the null hypothesis: Data appears to be normally distributed.")
+        #print("Fail to reject the null hypothesis: Data appears to be normally distributed.")
         return True
-    print("Reject the null hypothesis: Data is not normally distributed.")
+    #print("Reject the null hypothesis: Data is not normally distributed.")
     return False
+
 
 # sawilowsky2009new
 def effect_size_interpretation_sawilowsky(effect_size: float, dec_num: int = 2) -> str:
@@ -74,6 +93,35 @@ def cohen_d(group1, group2):
     print(interpretation)
 
 
+# cliff1993dominance
+def cliffs_delta(group1, group2):
+    # Measure of how often values in one distribution
+    # tend to be larger than values in another distribution.
+    x = np.array(group1)
+    y = np.array(group2)
+    Nx = len(x)
+    Ny = len(y)
+    bigger = 0
+    smaller = 0
+    for i in range(Nx):
+        for j in range(Ny):
+            if x[i] > y[j]:
+                bigger += 1
+            elif x[i] < y[j]:
+                smaller += 1
+    cd = (bigger - smaller) / float(Nx * Ny)
+    interpretation: str
+    if abs(cd) < 0.147:
+        interpretation = 'negligible'
+    elif abs(cd) < 0.33:
+        interpretation = 'small'
+    elif abs(cd) < 0.474:
+        interpretation = 'medium'
+    else:
+        interpretation = 'large'
+    print(f"Cliff's Delta: {cd} -> {interpretation} effect size")
+
+
 # gastwirth2009impact
 def equal_variances(group1, group2, alpha: float = 0.05) -> bool:
     if is_data_normally_distributed(group1) and is_data_normally_distributed(group2):
@@ -89,46 +137,70 @@ def equal_variances(group1, group2, alpha: float = 0.05) -> bool:
 
 def independent_ttest(group1, group2, alpha: float = 0.05) -> Optional[bool]:
     if is_data_normally_distributed(group1) and is_data_normally_distributed(group2):
+        print('Data is normally distributed -> Using t-test')
         if equal_variances(group1, group2):
             print("Variances are significantly different -> Using Welch's t-test.")
             equal_var = False
         else:
             print("Variances are NOT significantly different -> Using standard t-test.")
             equal_var = True
-        stat, p_value = ttest_ind(group1, group2, equal_var=equal_var)  # Null hypothesis: means of the two groups are equal
+        # Null hypothesis: means of the two groups are equal
+        stat, p_value = ttest_ind(group1, group2, equal_var=equal_var)
         print(f"t-statistic: {stat}, p-value: {p_value}")
         if p_value >= alpha:
-            print("Fail to reject the null hypothesis: NO significant difference between groups.")
+            print("Fail to reject the null hypothesis: suggests NO significant difference between groups.")
             return False
         else:
-            print("Reject the null hypothesis: Significant difference between groups.")
+            print("Reject the null hypothesis: statistically significant difference in the means.")
             cohen_d(group1, group2)
             return True
-    print('Data is not normally distributed')
     return None
 
 
-def pearson_correlation(group1, group2, alpha: float = 0.05) -> Optional[bool]:
-    assert len(group1) == len(group2)
-    if is_data_normally_distributed(group1) and is_data_normally_distributed(group2):
-        r, p_value = pearsonr(group1, group2)
-        print(f"Pearson Correlation = {r}, p-value = {p_value}")
-        if p_value >= alpha:
-            print("NO significant correlation between variables.")
-            return False
-        else:
-            print("Significant correlation between variables.")
-            print(f"r{effect_size_interpretation_funder_and_ozer(r)}")
-            return True
-    print('Data is not normally distributed')
-    return None
+def mannwhitneyu_test(group1, group2, alpha: float = 0.05) -> bool:
+    # Null hypothesis: The distributions of the two groups are the same with respect to stochastic dominance.
+    # Namely, the probability that a random value in one group is larger than a random value in the other.
+    # method='auto' -> 'exact' when len(group*) < 9 and there are no ties; chooses 'asymptotic' otherwise
+    U_stat, p_value = mannwhitneyu(group1, group2, alternative='two-sided', method='auto')
+    print(f"Mann–Whitney U statistic: {U_stat}, p-value: {p_value}")
+    if p_value >= alpha:
+        print("Fail to reject the null hypothesis: suggests NO significant difference between groups.")
+        return False
+    else:
+        print("Reject the null hypothesis: statistically significant difference in the distribution/location.")
+        cliffs_delta(group1, group2)
+        return True
 
 
-dist1 = np.random.normal(21, 8, 32)
-dist2 = np.random.normal(42, 10, 32)
-independent_ttest(dist1, dist2)
+def compare_groups(group1, group2, alpha: float = 0.05):
+    if len(group1) < 3 or len(group2) < 3:
+        print(f'Sample size too small: {group1=} {group2=} -> Collect more data!')
+        return
+    elif len(group1) < 11 or len(group2) < 11:
+        print(f'Small sample size" {group1=} {group2=} -> Using Mann–Whitney U Test.')
+        result = mannwhitneyu_test(group1, group2, alpha)
+    else:
+        result = independent_ttest(group1, group2, alpha)
+        if result is None:
+            print('Data is NOT normally distributed -> Using Mann–Whitney U Test.')
+            result = mannwhitneyu_test(group1, group2, alpha)
+    if not result:
+        print('Failing to reject the null hypothesis means you lack statistical evidence of a difference -> '
+              ' it is not proof of "no difference," it SUGGESTS that there is no difference.')
 
-print()
-dist3 = np.arange(10, 20)
-dist4 = np.array([5, 8, 12, 13, 18, 25, 40, 56, 66, 86])
-pearson_correlation(dist3, dist4)
+
+if __name__ == '__main__':
+    dist1 = np.random.normal(21, 8, 32)
+    dist2 = np.random.normal(42, 10, 32)
+    compare_groups(dist1, dist2)
+
+    print()
+
+    dist3 = np.random.uniform(low=1, high=10, size=32)
+    compare_groups(dist1, dist3)
+
+    print()
+
+    dist4 = np.arange(10, 20)
+    dist5 = np.array([5, 8, 12, 13, 18, 25, 40, 56, 66, 86])
+    pearson_correlation(dist4, dist5)
